d148df22a694c076f4ec80196a0cb77356352486
Miquel <miquel@catchoom.com>
2015-06-09 18:29:48 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## CraftAR - Android On-Device Image Recognition SDK examples
+
+### Introduction
+
+The CraftAR Service for [Augmented Reality and Image Recognition](http://catchoom.com/product/craftar/augmented-reality-and-image-recognition/) is a  service that allows you to build a wide range of __Image Recognition__ and __Augmented Reality__ applications
+and services.
+
+With CraftAR, you can create amazing apps that provide digital content
+for real-life objects like printed media, packaging among others. You
+can use our online web panel or APIs, to upload images to be recognized and set
+content to display upon recognition in your CraftAR-powered app.
+
+The [Android On-Device Image Recognition SDK](http://catchoom.com/documentation/on-device-image-recognition-sdk/android-on-device-image-recognition-sdk/) focuses on opening the camera in the mobile device and performing
+Image Recognition requests on the device. The display of the result of the request
+of each is up to you.
+
+This document describes mainly the Examples of different uses of the Service and the On Device Image Recognition SDK.
+General use of the SDK can be found in the Documentation webpage for the [Android On-Device Image Recognition SDK](http://catchoom.com/documentation/on-device-image-recognition-sdk/android-on-device-image-recognition-sdk/). Complete SDK documentation of the classes can be found within the distribution of the SDK itself.
+
+### How to use the examples
+
+This repository comes with a project of an Android app with several
+examples that show how to use the SDK.
+
+Eclipse:
+ 1. Import this project in Eclipse.
+ 2. Download and uncompress the [CraftAR On-Device Image Recognition SDK for Android](http://catchoom.com/product/craftar/augmented-reality-and-image-recognition-sdk/#download-mobile-sdk).
+ 3. Copy the libs/ and assets/ folders from the SDK in your application root directory.
+
+Android Studio:
+ 1.  Import this project in Android Studio.
+ 2. Download and uncompress the [CraftAR On-Device Image Recognition SDK for Android](http://catchoom.com/product/craftar/augmented-reality-and-image-recognition-sdk/#download-mobile-sdk).
+ 3.  In Android studio, select the view 'Project Files' in the project explorer.
+ 4.  Create a folder 'libs' in the root of the project. Copy all the .jars included in the libs folder of our SDK there.
+ 5.  Create a folder jniLibs in the src/main folder. Copy the folders armeabi-v7a and x86 there.
+ 6.  Copy the assets/ folder in your application root directory.
+ 7.  If you have import errors with the CraftAR classes, right click on each of the jars in libs/ folder and click on 'Add as library'
+
+* Note: If your app uses android-support libraries, remove the android-support-v13.jar from the libs folder, and use your own one instead.
---
+++ src/com/craftar/examples/SplashScreenActivity.java
@@ -0,0 +1,135 @@
+// com.craftar.craftarexamplesir is free software. You may use it under the MIT license, which is copied
+// below and available at http://opensource.org/licenses/MIT
+//
+// Copyright (c) 2014 Catchoom Technologies S.L.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+package com.craftar.examples;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.widget.Toast;
+
+import com.catchoom.test.R;
+import com.craftar.CraftARCollection;
+import com.craftar.CraftARCollectionManager;
+import com.craftar.CraftARCollectionManager.AddCollectionListener;
+import com.craftar.CraftARError;
+import com.craftar.CraftAROnDeviceIR;
+import com.craftar.SetCollectionListener;
+
+public class SplashScreenActivity extends Activity implements SetCollectionListener,
+AddCollectionListener{
+
+	private final static String TAG = "SplashScreenActivity";	
+
+	//Collection token of the collection you want to load.
+	//Note that you can load several collections at once, but every search 
+	//request is performed only on ONE collection (the one that you have set through CraftAROnDeviceIR.setCollection()).
+	public final static String COLLECTION_TOKEN="catchoomcooldemo";
+
+	CraftAROnDeviceIR mCraftAROnDeviceIR;
+	CraftARCollectionManager mCollectionManager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.splash_screen);
+
+		//Initialize the Offline IR Module
+		mCraftAROnDeviceIR = CraftAROnDeviceIR.Instance(getApplicationContext());
+				
+		//Initialize the Collection Manager
+		mCollectionManager = CraftARCollectionManager.Instance(getApplicationContext()); 
+				
+		//Obtain the collection with your token.
+		//This will lookup for the collection in the internal storage, and return the collection if it's available.
+		CraftARCollection col =  mCollectionManager.get(COLLECTION_TOKEN); 
+		if(col == null){
+			//Collection is not available. Add it from assets using the collection bundle.
+			mCollectionManager.addCollection((AddCollectionListener)this,"catchoomcooldemoBundle.zip");
+		}else{
+			//Collection is already available in the device.
+			loadCollection(col);
+		}
+    }
+
+	@Override
+	public void collectionReady() {
+		//Collection is ready for recognition.
+		Intent launchersActivity = new Intent( SplashScreenActivity.this, LaunchersActivity.class);
+		startActivity(launchersActivity);
+		finish();
+	}
+
+	@Override
+	public void setCollectionFailed(CraftARError error) {
+		//Error loading the collection into memory. No recognition can be performed unless a collection has been set.
+		Log.e(TAG,"SetCollectionFailed ("+error.getErrorCode()+"):"+error.getErrorMessage());
+		Toast.makeText(getApplicationContext(), "Error loading", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void setCollectionProgress(double progress) {
+		//The images from the collection are loading into memory. You will have to load the collections into memory every time you open the app. 
+		Log.d(TAG,"SetCollectionProgress:"+progress);
+	}
+
+
+	@Override
+	public void collectionAdded(CraftARCollection collection) {
+		//Collection bundle has been added. Set this collection as current collection.
+		loadCollection(collection);
+	}
+	
+	@Override
+	public void addCollectionFailed(CraftARError error) {
+		//Error adding the bundle to the device internal storage. 
+		Log.e(TAG,"AddCollectionFailed("+error.getErrorCode()+"):"+error.getErrorMessage());
+		Toast.makeText(getApplicationContext(), "Error adding collection", Toast.LENGTH_SHORT).show();
+		switch(error.getErrorCode()){
+		case COLLECTION_BUNDLE_SDK_VERSION_IS_OLD:
+			//You are trying to add a bundle which version is newer than the SDK version. 
+			//You should either update the SDK, or download and add a bundle compatible with this SDK version.
+			break;
+		case COLLECTION_BUNDLE_VERSION_IS_OLD:
+			//You are trying to add a bundle which is outdated, since the SDK version is newer than the bundleSDK 
+			//You should download a bundle compatible with the newer SDK version.
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	public void addCollectionProgress(float progress) {
+		//Progress adding the collection to internal storage (de-compressing bundle and storing into the device storage).
+		//Note that this might only happen once per app installation, or when the bundle is updated.
+		Log.d(TAG,"AddCollectionProgress:"+progress);
+	}
+	
+	private void loadCollection(CraftARCollection collection){
+		mCraftAROnDeviceIR.setCollection((SetCollectionListener)this, collection);
+	}
+
+}
