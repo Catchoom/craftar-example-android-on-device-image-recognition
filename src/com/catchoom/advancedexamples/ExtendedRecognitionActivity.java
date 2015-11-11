a17ffcf25e2776b2dec67652fd6188f8b0cbcef9
Miquel Ferrarons Betrian <miquelferrarons@gmail.com>
2015-11-11 19:07:00 +0100

    Simplify example with ExtendedSearchController

--- src/com/catchoom/advancedexamples/ExtendedRecognitionActivity.java
+++ src/com/catchoom/advancedexamples/ExtendedRecognitionActivity.java
@@ -74,7 +74,7 @@ import com.craftar.SearchController;
  * in the cloud will be performed. In the case you were pointing to the kid_with_mobile or to the craftar_logo images, the search in the cloud
  * will find the match.
  * **/
-public class ExtendedRecognitionActivity extends CraftARActivity implements OnClickListener{
+public class ExtendedRecognitionActivity extends CraftARActivity implements OnClickListener, CraftARSearchResponseHandler{
 
 	private final static String TAG = "ExtendedRecognitionActivity";
 
@@ -84,29 +84,11 @@ public class ExtendedRecognitionActivity extends CraftARActivity implements OnCl
 	private View mScanningLayout;
 	private View mTapToScanLayout;
 		
-	CraftAROnDeviceIR mOnDeviceIR; //The On-Device image recognition module
-	CraftARCloudRecognition mCloudIR; //The Cloud Image recognition module
-	CraftAROnDeviceCollectionManager mOnDeviceCollectionManager; //The On-Device collection manager, used to manage the on-device collections
-	
-	MyCloudSetCollectionListener mCloudSetCollectionListener; //The listener for the setCollection call of the CraftARCloudRecognition module
-	MyOnDeviceSetCollectionListener mOnDeviceSetCollectionListener; //The listener for the setCollection call of the CraftAROnDeviceIR module
-	MyOnDeviceAddCollectionListener mOnDeviceAddCollectionListener; //The listener for the addCollection call of the CraftAROnDeviceIR module
-	
-	//Obtains the pictures and frames of the camera, and the finder events. 
-	//It will be used to forward the searches to the corresponding module.
-	MySearchController mSearchController; 
-
-	CloudResponseHandler mCloudResponseHandler; // Listener for the search() requests of the CraftARCloudRecognition module
-	OnDeviceResponseHandler mOnDeviceResponseHandler; // Listener for the search() requests of the CraftAROnDeviceIR module
-	
 	CraftARSDK mCraftARSDK; //The CraftARSDK object.
 	CraftARCamera mCamera; //Provides high-level access to some features of the device camera.
 	
 	private boolean mIsActivityRunning;
-	
-	boolean mOnDeviceReady = false; //Flag to control if the call to setCollection in the CraftAROnDeviceIR module has been completed successfully.
-	boolean mCloudReady = false; //Flag to control if the call to setCollection in the CraftARCloudRecognition module has been completed successfully.
-	
+		
 	@Override
 	public void onPostCreate() {
 
@@ -121,75 +103,61 @@ public class ExtendedRecognitionActivity extends CraftARActivity implements OnCl
 		mCraftARSDK.init(getApplicationContext()); //Initialize always the SDK before doing any other operation. If the SDK has already been initialized, this is a no-op.
 		mCraftARSDK.startCapture((CraftARActivity)this); //Starts the camera capture.
 		
-		onDeviceSetup(); //Setup the CraftAROnDeviceIR module
-		cloudSetup(); //Setup the CraftARCloudRecognition module
+		CraftARCloudRecognition cloudIR = CraftARCloudRecognition.Instance(); //Get the instance to the CraftARCloudRecognition module		
+		//Use the collection specified by the TOKEN in the CraftARCloudRecognition module. Receive the callbacks from the setCollection call in our CloudSetCollectionListener 
+		cloudIR.setCollection(MY_CLOUD_COLLECTION_TOKEN, new SetCollectionListener() {
+			@Override
+			public void collectionReady() {
+				Log.d(TAG, "Cloud collection is ready!");
+				mScanningLayout.setVisibility(View.GONE);
+				mTapToScanLayout.setVisibility(View.VISIBLE);
+			}
+
+			@Override
+			public void setCollectionFailed(CraftARError error) {
+				Toast.makeText(getApplicationContext(), "Error setting cloud collection:"+error.getErrorMessage(), Toast.LENGTH_SHORT).show();
+			}
+		}); 		
 		
-		mSearchController = new MySearchController();
+		ExtendedSearchController extendedSearchController = new ExtendedSearchController((CraftARSearchResponseHandler)this);
 		
 		//Set the SearchController in the SDK. By doing this, the SDK will forward the pictures, the frames, and the finder events to our SearchController.
-		mCraftARSDK.setSearchController(mSearchController); 
+		mCraftARSDK.setSearchController(extendedSearchController); 
 		mCamera = mCraftARSDK.getCamera(); //Obtain the camera object from the SDK.
 	}
-
-	private void onDeviceSetup(){
-
-		mOnDeviceReady = false;
-		mOnDeviceIR = CraftAROnDeviceIR.Instance();	//Get the instance to the CraftAROnDeviceIR module		
-
-		mOnDeviceResponseHandler = new OnDeviceResponseHandler(); //Create our SetCollectionListener for the CraftAROnDeviceIR module.
-		mOnDeviceSetCollectionListener = new MyOnDeviceSetCollectionListener();  //Create our SetCollectionListener for the CraftAROnDeviceIR module.
-		mOnDeviceAddCollectionListener = new MyOnDeviceAddCollectionListener();  //Create an AddCollectionListener for the CraftAROnDeviceIR module.
-		
-		mOnDeviceIR.setCraftARSearchResponseHandler(mOnDeviceResponseHandler ); //Tell the SDK that we want to receive the on-device search responses in the OnDeviceResponseHandler
-
-		mOnDeviceCollectionManager = CraftAROnDeviceCollectionManager.Instance(); //Obtain the on-device collection manager.
-		CraftAROnDeviceCollection collection = mOnDeviceCollectionManager.get(MY_ONDEVICE_COLLECTION_TOKEN); // Try to obtain the local collection for our token
-		
-		//Check if the collection could be obtained
-		if(collection != null){ 
-			//The collection was available in the device, use this collection in the CraftAROnDeviceIR module.
-			mOnDeviceIR.setCollection(collection, mOnDeviceSetCollectionListener);
-		}else{
-			//The collection was not available in the device, add this collection.
-			mOnDeviceCollectionManager.addCollectionWithToken(MY_ONDEVICE_COLLECTION_TOKEN, mOnDeviceAddCollectionListener);
-		}
-		
-	}
 	
-	private void cloudSetup(){
-
-		mCloudReady = false; 
-		mCloudIR = CraftARCloudRecognition.Instance(); //Get the instance to the CraftARCloudRecognition module		
-		mCloudResponseHandler = new CloudResponseHandler(); //Create our CraftARSearchResponseHandler for the CraftARCloudRecognition module.
-		mCloudSetCollectionListener = new MyCloudSetCollectionListener(); //Create our SetCollectionListener for the CraftARCloudRecognition module.
-
-		mCloudIR.setCraftARSearchResponseHandler(mCloudResponseHandler); //Tell the SDK that we want to receive the search responses from the Cloud in the CloudResponseHandler
-		//Use the collection specified by the TOKEN in the CraftARCloudRecognition module. Receive the callbacks from the setCollection call in our CloudSetCollectionListener 
-		mCloudIR.setCollection(MY_CLOUD_COLLECTION_TOKEN, mCloudSetCollectionListener); 
-	}
 	@Override
 	public void onCameraOpenFailed() {
-		//Error opening the camera
-		Toast.makeText(getApplicationContext(), "Camera error", Toast.LENGTH_SHORT).show();
+		Toast.makeText(getApplicationContext(), "Camera error", Toast.LENGTH_SHORT).show();		
 	}
 
 	@Override
 	public void onPreviewStarted(int width, int height) {
-		Log.d(TAG, "Camera preview started successfully");
+		// TODO Auto-generated method stub
+		
 	}
-	
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
-			case R.id.tap_to_scan:
-				mTapToScanLayout.setVisibility(View.GONE);
-				mScanningLayout.setVisibility(View.VISIBLE);
-				mCraftARSDK.singleShotSearch(); //Request a picture to the SDK. Since we are implementing our custom SearchController, this will produce a callback to the method
-				// onPictureTaken(CraftARQueryImage queryImage) of our SearchController.
-				break;
+		if (v == mTapToScanLayout) {
+			mTapToScanLayout.setVisibility(View.GONE);
+			mScanningLayout.setVisibility(View.VISIBLE);
+			mCraftARSDK.singleShotSearch();
 		}
 	}
-	
+
+	@Override
+	public void searchResults(ArrayList<CraftARResult> result,
+			long searchTimeMillis, int requestCode) {
+		//Callback with the search results
+		if(result.size()> 0){
+			//We found something! Show the results
+			showResultDialog(result);
+		}else{
+			//No objects found for this request
+			showNoObjectsDialog();
+		}
+	}
+
 	private void showNoObjectsDialog(){
 		if(!mIsActivityRunning){
 			return;
@@ -197,7 +165,7 @@ public class ExtendedRecognitionActivity extends CraftARActivity implements OnCl
 		
 		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
 		dialogBuilder.setTitle("No objects found");
-		dialogBuilder.setMessage("Point to an object of the collection " +MY_ONDEVICE_COLLECTION_TOKEN + " or " + MY_CLOUD_COLLECTION_TOKEN);
+		dialogBuilder.setMessage("Point to an object of the "+SplashScreenActivity.COLLECTION_TOKEN+" collection");
 		dialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) { 
 	        	showTapToScan();
@@ -235,6 +203,15 @@ public class ExtendedRecognitionActivity extends CraftARActivity implements OnCl
 		dialogBuilder.show();
 	}
 	
+	@Override
+	public void searchFailed(CraftARError error, int requestCode) {
+		mScanningLayout.setVisibility(View.GONE);
+		mTapToScanLayout.setVisibility(View.VISIBLE);		
+		Log.e(TAG, "Search failed("+error.getErrorCode()+"):"+error.getErrorMessage());
+		//Some error occurred. We just show a toast with the error
+		mCamera.restartCapture();		
+	}
+	
 	private void showTapToScan(){
 		mCamera.restartCapture();		
 		mScanningLayout.setVisibility(View.GONE);
@@ -253,168 +230,5 @@ public class ExtendedRecognitionActivity extends CraftARActivity implements OnCl
 		mIsActivityRunning = true;
 		
 	}
-	
-	private void onSearchFailed(CraftARError error){
-		mScanningLayout.setVisibility(View.GONE);
-		mTapToScanLayout.setVisibility(View.VISIBLE);		
-		Log.e(TAG, "Search failed("+error.getErrorCode()+"):"+error.getErrorMessage());
-		//Some error occurred. We just show a toast with the error
-		mCamera.restartCapture();	
-	}
-
-	class OnDeviceResponseHandler implements CraftARSearchResponseHandler{
-
-		@Override
-		public void searchResults(ArrayList<CraftARResult> results,
-				long searchTimeMillis, int requestCode) {
-			//Callback with the search results
-			if(results.size()> 0){
-				//We found something! Show the results
-				showResultDialog(results);
-			}else{
-				if(mCloudReady){
-					Toast.makeText(getApplicationContext(), "Nothing found locally,  searching on cloud...", Toast.LENGTH_SHORT).show();
-					//No objects found locally for this request, search it on the cloud!
-					CraftARQueryImage lastQueryImage = mSearchController.getLastQueryImage();
-					if(lastQueryImage != null){
-						mCloudIR.search(lastQueryImage);
-					}
-				}else{
-					Toast.makeText(getApplicationContext(), "Cloud not ready", Toast.LENGTH_SHORT).show();
-					showNoObjectsDialog();
-				}
-			}			
-		}
-
-		@Override
-		public void searchFailed(CraftARError error, int requestCode) {
-			onSearchFailed(error);				
-		}
-		
-	}
-	
-	class CloudResponseHandler implements CraftARSearchResponseHandler{
-
-		@Override
-		public void searchResults(ArrayList<CraftARResult> results,
-				long searchTimeMillis, int requestCode) {
-			if(results.size()> 0){
-				//We found something! Show the results
-				showResultDialog(results);
-			}else{
-				//No objects found for this request
-				showNoObjectsDialog();
-			}	
-		}
-
-		@Override
-		public void searchFailed(CraftARError error, int requestCode) {
-			onSearchFailed(error);
-		}
-		
-	}
-	
-	class MySearchController implements SearchController{
-
-		CraftARQueryImage mLastQuery;
-		boolean mIsFinding = false;
-		public CraftARQueryImage getLastQueryImage(){
-			return mLastQuery;
-		}
-		
-		@Override
-		public void onPictureTaken(CraftARQueryImage image) {
-			mLastQuery = image;
-			if(mOnDeviceReady){
-				mOnDeviceIR.search(image);
-			}
-		}
-
-		@Override
-		public void onTakePictureFailed(CraftARError error) {
-			mOnDeviceResponseHandler.searchFailed(error, 0);
-		}
-
-		@Override
-		public void onPreviewFrame(CraftARQueryImage image) {
-			if(mIsFinding){
-				//TODO: If you want to use Finder mode, implement your logic here!
-				//Example: Search this frame only when the previous request was finished.
-				if(mOnDeviceIR.getPendingSearchRequestsCount() == 0 ){
-					mOnDeviceIR.search(image);
-				}
-			}
-		}
-
-		@Override
-		public void onFinderActivated() {
-			mIsFinding = true;
-		}
-
-		@Override
-		public void onFinderDeactivated() {
-			mIsFinding = false;
-		}
-		
-	}
-	
-	class MyOnDeviceSetCollectionListener implements SetOnDeviceCollectionListener{
-
-		@Override
-		public void collectionReady() {
-			Log.d(TAG, "OnDevice collection is ready!");
-			mOnDeviceReady = true;
-			mScanningLayout.setVisibility(View.GONE);
-			mTapToScanLayout.setVisibility(View.VISIBLE);
-		}
-
-		@Override
-		public void setCollectionFailed(CraftARError error) {
-			Toast.makeText(getApplicationContext(), "Error setting on-device collection:"+error.getErrorMessage(), Toast.LENGTH_SHORT).show();			
-		}
-
-		@Override
-		public void setCollectionProgress(double progress) {
-			Log.d(TAG,"Progress loading on-device collection:"+progress);
-		}
-		
-	}
-	
-	class MyCloudSetCollectionListener implements SetCollectionListener{
-
-		@Override
-		public void collectionReady() {
-			Log.d(TAG, "Cloud collection is ready!");
-			mCloudReady = true;
-		}
-
-		@Override
-		public void setCollectionFailed(CraftARError error) {
-			Toast.makeText(getApplicationContext(), "Error setting cloud collection:"+error.getErrorMessage(), Toast.LENGTH_SHORT).show();
-		}
-		
-	}
-	
-	/** Listener used when adding an on-device collection in the device. */
-	class MyOnDeviceAddCollectionListener implements AddCollectionListener{
-
-		@Override
-		public void collectionAdded(CraftAROnDeviceCollection collection) {
-			mOnDeviceIR.setCollection(collection, mOnDeviceSetCollectionListener);	
-		}
-
-		@Override
-		public void addCollectionFailed(CraftARError error) {
-			Toast.makeText(getApplicationContext(), "Error adding on-device collection:"+error.getErrorMessage(), Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void addCollectionProgress(float progress) {
-			Log.d(TAG,"Progress adding on-device collection:"+progress);
-			
-		}
-		
-	}
-	
 
 }
