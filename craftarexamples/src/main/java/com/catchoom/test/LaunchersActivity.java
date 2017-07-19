703672ff2baee49d6195c2105dfbf87bcceaa0d1
Luis Martinell Andreu <luis@catchoom.com>
2017-07-19 15:13:28 +0200

    Request camera permissions at runtime.

--- craftarexamples/src/main/java/com/catchoom/test/LaunchersActivity.java
+++ craftarexamples/src/main/java/com/catchoom/test/LaunchersActivity.java
@@ -23,13 +23,20 @@
 package com.catchoom.test;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.catchoom.advancedexamples.ExtendedRecognitionActivity;
+import com.craftar.CraftARSDK;
 
 public class LaunchersActivity extends Activity implements OnClickListener {
 	
@@ -38,6 +45,7 @@ public class LaunchersActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_launchers);
+		CraftARSDK.Instance().init(getApplicationContext());
 
 		// Setup example links
 		findViewById(R.id.play_finder).setOnClickListener(this);
@@ -79,9 +87,75 @@ public class LaunchersActivity extends Activity implements OnClickListener {
 		}
 		
 		if (intent != null) {
-			startActivity(intent);
+			final Intent finalIntent = intent;
+			checkPermissionAndAskIfNotGranted(new Runnable() {
+				@Override
+				public void run() {
+					startActivity(finalIntent);
+				}
+			});
 			return;
 		}
+	}
+
+
+
+	private static final int CAMERA_PERMISSION = 0;
+
+	private Runnable doWhenGranted;
+
+	public void checkPermissionAndAskIfNotGranted(Runnable doWhenGranted) {
+		this.doWhenGranted = doWhenGranted;
+		if (PermissionChecker.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PermissionChecker.PERMISSION_GRANTED) {
+
+			// Should we show an explanation?
+			if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+					android.Manifest.permission.CAMERA)) {
+				showExplanation("Camera access", "This app needs to use the camera to demostrate the SDK's capabilities", android.Manifest.permission.CAMERA, CAMERA_PERMISSION);
+			} else {
+				requestPermission(android.Manifest.permission.CAMERA, CAMERA_PERMISSION);
+			}
+		} else {
+			doWhenGranted.run();
+		}
+	}
+
+	private void showExplanation(String title,
+								 String message,
+								 final String permission,
+								 final int permissionRequestCode) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(title)
+				.setMessage(message)
+				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						requestPermission(permission, permissionRequestCode);
+					}
+				});
+		builder.create().show();
+	}
+
+	private void requestPermission(String permissionName, int permissionRequestCode) {
+		ActivityCompat.requestPermissions(this,
+				new String[]{permissionName}, permissionRequestCode);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode,
+										   String permissions[], int[] grantResults) {
+		switch (requestCode) {
+			case CAMERA_PERMISSION: {
+				// If request is cancelled, the result arrays are empty.
+				if (grantResults.length > 0
+						&& grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					doWhenGranted.run();
+				} else {
+					Toast.makeText(getApplicationContext(), "Sorry, without camera permission, the examples will not work.", Toast.LENGTH_SHORT).show();
+				}
+				return;
+			}
+		}
+
 	}
 
 }
